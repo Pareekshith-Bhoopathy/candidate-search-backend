6eb63887a8eac8f967c289cbddc9b1fd830299ca
Pareekshith-Bhoopathy <pareekshith.bhoopathy@edgaadi.com>
2024-09-22 21:50:00 +0530

    cors fix

--- src/server.js
+++ src/server.js
@@ -2,13 +2,26 @@
 const app = require('./app');
 const config = require('./config/config');
 const logger = require('./utils/logger');
+const cors = require('cors');
 
+app.use(cors());
 // Ensure upload directory exists
 const fs = require('fs');
 if (!fs.existsSync(config.uploadDir)) {
   fs.mkdirSync(config.uploadDir, { recursive: true });
 }
 
+
+const corsOptions = {
+  origin: '*', // Allow all origins
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
+
+
+app.use(cors(corsOptions));
 // Start Server
 app.listen(config.port, () => {
   logger.info(`Server is running on port ${config.port}`);
